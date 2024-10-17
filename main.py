cbe0f76ee189f34c111f6488ae5aa748c01f6c2a
axi <77366347+axisdadev@users.noreply.github.com>
2024-10-17 15:49:02 -0700

    Create main.py

--- main.py
+++ main.py
@@ -19,22 +19,27 @@ intents.guilds = True
 
 if defaultConfig["LOGGING"] is True:
     logger = logging.getLogger("nextcord")
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(logging.INFO)
 
-    handler = logging.FileHandler(config["LOG-FILE"])
+    handler = logging.FileHandler(defaultConfig["LOG-FILE"])
     handler.setFormatter(
         fmt=logging.Formatter("%(asctime)s:%(levelname)s:%(name)s: %(message)s")
     )
 
-    logger.addHandler()
+    logger.addHandler(handler)
 
 
 ## Bot Construction
 
-bot = commands.bot(intents=intents)
+bot = commands.Bot(intents=intents)
 
 for filename in os.listdir("./ext"):  # Use ./ext as a folder to contain all Cogs (Commands)
     if filename.endswith(".py"):  
         bot.load_extension(f"ext.{filename[:-3]}")
+
+
+@bot.event
+async def on_ready():
+    logger.info(f"Running OpenRewards Release Version - {defaultConfig['RELEASE']}\nThank you for using OpenRewards!")
 
 bot.run(defaultConfig['BOT-TOKEN'])
