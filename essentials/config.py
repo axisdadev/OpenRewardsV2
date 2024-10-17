fc1c2a441c8be45f24e32599858a933c5c822a07
axi <77366347+axisdadev@users.noreply.github.com>
2024-10-17 15:24:16 -0700

    Backup

--- essentials/config.py
+++ essentials/config.py
@@ -1,16 +1,26 @@
 import yaml
 
+
 class ConfigurationManager:
     def __init__(self):
         with open("configurations/bot.yml") as f:
-            yamlFile = yaml.load(f)
+            yamlFile = yaml.safe_load(f)
             self.botConfig = yamlFile
-    
+
         return
-    
+
     def getBotConfig(self):
         """Get the default configuration set for the bot upon launch."""
         return self.botConfig
-    
-    def getCommandConfig(self):
-        """Get a configuration for a specific command. Must be a valid configuration"""
+
+    def getCommandConfig(self, name):
+        """Get a configuration for a specific command. Must be a valid configuration file in .yml"""
+
+        filePath = f"configurations/commands/{name}.yml"
+        result = None
+
+        with open(file=filePath) as f:
+            yamlFile = yaml.safe_load(f)
+            result = yamlFile
+
+        return result
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import nextcord, os, logging
 from nextcord.ext import commands
+from essentials import config
+
+configurationManager = config.ConfigurationManager()
+defaultConfig = configurationManager.getBotConfig()
 
