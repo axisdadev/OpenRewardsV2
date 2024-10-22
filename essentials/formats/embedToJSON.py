ef793579ecd1460b31a9fbb477ae50be43494c87
axi <77366347+axisdadev@users.noreply.github.com>
2024-10-21 20:29:53 -0700

    Update embedToJSON.py

--- essentials/formats/embedToJSON.py
+++ essentials/formats/embedToJSON.py
@@ -1,7 +1,7 @@
 import json
 import nextcord
 
-def json_to_nextcord_embed(json_string):
+def jsonToEmbed(json_string):
     # Parse the JSON string into a Python dictionary
     embed_data = json.loads(json_string)
 
@@ -30,25 +30,3 @@ def json_to_nextcord_embed(json_string):
             embed.add_field(name=field.get("name", ""), value=field.get("value", ""), inline=field.get("inline", True))
 
     return embed
-
-# Example JSON string for testing
-json_string = '''
-{
-    "title": "This is a test embed",
-    "description": "Description of the embed",
-    "color": 15158332,
-    "fields": [
-        {"name": "Field 1", "value": "This is field 1", "inline": true},
-        {"name": "Field 2", "value": "This is field 2", "inline": false}
-    ],
-    "footer": {"text": "Footer text", "icon_url": "https://example.com/footer.png"},
-    "thumbnail": {"url": "https://example.com/thumbnail.png"},
-    "author": {"name": "Author Name", "icon_url": "https://example.com/author.png"}
-}
-'''
-
-# Convert the JSON to a nextcord embed
-embed = json_to_nextcord_embed(json_string)
-
-# Now you can send the embed using a bot, for example in an on_message event
-# await message.channel.send(embed=embed)
